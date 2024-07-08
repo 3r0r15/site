3af76d312ab8a645895856c432dca9963c10785b
3r0r15 <kirilltank140@gmail.com>
2024-07-08 16:16:54 +0300

    index and prod

--- int_site/ste/urls.py
+++ int_site/ste/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-
+    path("products/<int:id>", views.view_product, name="view_product"),
 ]
--- int_site/ste/views.py
+++ int_site/ste/views.py
@@ -2,9 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from .models import Product
+
 # Create your views here.
 def home(request):
-    products = Product.objects.all()
+    prod = Product.objects.all()
     return render(request, "index.html",{
-        'products': products
+        'prod': prod
     })
+
+def view_product(request, id):
+    return render(request, 'products.html',)
